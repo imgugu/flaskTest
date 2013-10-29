7c9515d52e551c24275373cbe96dffdcee90be15
jlgu <xqg8484@163.com>
2013-10-29 17:27:04 +0800

    ..

--- app/views.py
+++ app/views.py
@@ -1,9 +1,17 @@
-from flask import render_template
+from flask import render_template, session, request, redirect, url_for
 from app import app
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index/", methods=["GET"])
 def index():
     user = { 'nickname': 'Miguel' } # fake user
     friends = [{'name':'Jim Green','sex':'boy'},{'name':'Han Meimei','sex':'girl'}]
-    return render_template("index.html", title = 'Home', user = user, friends = friends, c = len(friends))
+    return render_template("index.html", title = 'Home', user=user, friends=friends, c = len(friends))
+
+@app.route("/index/", methods=["POST"])
+def indexPost():
+    return redirect(url_for('home',xx=request.form['xx']))
+
+@app.route('/home/<xx>/')
+def home(xx):
+    return render_template("home.html", x=xx)
